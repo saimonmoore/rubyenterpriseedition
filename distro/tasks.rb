12c1be7f173f51216b883409fee2ca6cb7ed68c6
Saimon Moore <saimon@saimonmoore.net>
2009-03-30 15:04:30 +0200

    Don't bother with make install...use install command directly

--- distro/tasks.rb
+++ distro/tasks.rb
@@ -68,7 +68,7 @@ task 'package:debian' => :fakeroot do
   sh "ruby fakeroot/usr/local/src/shadow-1.4.1/extconf.rb"
   sh "mv Makefile fakeroot/usr/local/src/shadow-1.4.1/"
   sh "make -C fakeroot/usr/local/src/shadow-1.4.1"
-  sh "DESTDIR=fakeroot && make -e -C fakeroot/usr/local/src/shadow-1.4.1 install"
+  sh "/usr/bin/install -c -m 0755 shadow.so fakeroot/usr/local/lib/ruby/lib/ruby/site_ruby/1.8/x86_64-linux"
 
   sh "sed -i 's/Version: .*/Version: #{VENDOR_RUBY_VERSION}-#{REE_VERSION}/' fakeroot/DEBIAN/control"
   sh "sed -i 's/Architecture: .*/Architecture: #{arch}/' fakeroot/DEBIAN/control" 
